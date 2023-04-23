1f5598ce38243b277675e4f768d555dc72e7e08c
admin <dmitry.korzhenevsky@yandex.ru>
2023-04-23 17:36:02 +0300

    added hw4

--- main3.py
+++ main3.py
@@ -83,7 +83,7 @@ Q, Z – 10 очков.
     
 
 
-lang = str(input("Please, you should choice languadge you will use (en OR rus): "))
+""" lang = str(input("Please, you should choice languadge you will use (en OR rus): "))
 sum = 0
 if lang == 'en':
     str_en = str(input("Please, write here any word: "))
@@ -99,4 +99,82 @@ else:
     for i in str_rus:
         if dict_rus.get(i):
             sum += int(dict_rus[i])
-    print(f"Вы набрали {sum} очков!")
+    print(f"Вы набрали {sum} очков!") """
+
+
+
+""" 25. Напишите  программу,  которая  принимает  на  вход строку,  и  отслеживает,  сколько  раз  каждый  символ 
+уже встречался. Количество повторов добавляется к символам с помощью постфикса формата _n.
+Input: a a a b c a a d c d d
+Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2 """
+
+
+""" import random
+
+some_str = ''.join([chr(random.randint(32, 100)) for _ in range(10 ** 5)])
+
+import time
+
+start = time.perf_counter()
+for letter in set(some_str):
+    a = letter, some_str.count(letter)
+end = time.perf_counter()
+duration1 = end - start
+
+
+start = time.perf_counter()
+for letter in set(some_str):
+    amount = 0
+    for letter1 in some_str:
+        if letter == letter1:
+            amount += 1
+    a = letter, amount
+end = time.perf_counter()
+duration2 = end - start
+
+
+start = time.perf_counter()
+count = 0
+lens = len(some_str)
+while lens > 0:
+    for i in range(0, lens):
+        if some_str[0] == some_str[i]:
+            count += 1
+    lens -= count
+    a = f'{some_str[0]} -> {count}'
+    some_str = some_str.replace(some_str[0], '')
+    count = 0
+end = time.perf_counter()
+duration3 = end - start
+
+
+start = time.perf_counter()
+count_dict = {}  # a: 1
+for letter in some_str:
+    if letter not in count_dict:
+        count_dict[letter] = 1
+    else:
+        count = count_dict[letter]
+        count_dict[letter] = count + 1
+end = time.perf_counter()
+duration4 = end - start
+print(duration1, duration2, duration3, duration4) """
+
+
+""" 27. Пользователь вводит текст(строка). Словом считается последовательность непробельных символов идущих подряд,
+слова разделены одним или большим числом пробелов или символами конца строки.Определите, сколько различных
+слов содержится в этом тексте. без метода split() """
+
+
+""" some_str = input()
+some_set = set()
+temp_word = ''
+for letter in some_str:
+    if letter != ' ':
+        temp_word += letter
+    else:
+        if not temp_word:
+            some_set.add(temp_word)
+        temp_word = ''
+some_set.add(temp_word)
+print(some_set) """
---
+++ main4.py
@@ -0,0 +1,120 @@
+""" Задача 22: Даны два неупорядоченных набора целых чисел (может быть, с повторениями). 
+Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов 
+второго множества. Затем пользователь вводит сами элементы множеств. """
+
+""" import random
+    
+n = int(input("Please, insert the quantity of first array's elements: "))
+list_first = [random.randint(1, 10) for i in range (n)]
+list_first.sort()
+print(list_first)
+m = int(input("Please, insert the quantity of second array's elements: "))
+list_second = [random.randint(1, 10) for i in range (m)]
+list_second.sort()
+print(list_second)
+set_first = set(list_first)
+set_second = set(list_second)
+set_new = set_first.union(set_second) 
+print(set_new) """
+
+
+""" Задача 24: В фермерском хозяйстве в Карелии выращивают чернику. Она растёт на круглой грядке, 
+причём кусты высажены только по окружности. Таким образом, у каждого куста есть ровно два соседних. 
+Всего на грядке растёт N кустов. Эти кусты обладают разной урожайностью, поэтому ко времени сбора 
+на них выросло различное число ягод — на i-ом кусте выросло ai ягод. В этом фермерском хозяйстве 
+внедрена система автоматического сбора черники. Эта система состоит из управляющего модуля и 
+нескольких собирающих модулей. Собирающий модуль за один заход, находясь непосредственно перед 
+некоторым кустом, собирает ягоды с этого куста и с двух соседних с ним. Напишите программу для 
+нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль, 
+находясь перед некоторым кустом заданной во входном файле грядки."""
+
+
+import random
+n = int(input("Please, insert the quantity of blueberry bushes: "))
+list = [random.randint(80, 100) for i in range(n)]
+print(list)
+sum = max = i = 0
+while i + 2 < n:
+    sum = list[i] + list[i + 1] + list[i + 2]
+    print(sum, i, max)
+    if sum >= max:
+        max = sum
+        i = i + 1
+        sum = 0
+    else:
+        i = i + 1
+        sum = 0
+else:
+    k = 3
+    while k > 0:
+        sum = list[n-k] + list[n - (k - 1)] + list[i - (k + 1)]
+        print(sum, i, max)
+        if sum >= max:
+            max = sum
+            i = i + 1
+            k = k - 1
+            sum = 0
+        else:
+            i = i + 1
+            k = k - 1
+            sum = 0
+print(max)
+
+
+""" Задача №33. Хакер Василий получил доступ к классному журналу и хочет заменить все свои минимальные оценки на
+максимальные. Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
+Input: 5 -> 1 3 3 3 4
+Output: 1 3 3 3 1 """
+
+""" import random 
+
+number_list = int(input("Введите количтсво оценок: "))
+some_list = [random.randint(1, 5) for i in range (number_list)]
+
+min = max = some_list[0]
+
+for i in some_list:
+    if min > i:
+          min = i
+    elif max < i:
+          max = i
+
+print(*some_list)
+
+for i in range (len(some_list)):
+    if some_list[i] == max:
+        some_list[i] = min
+
+print(*some_list)
+ """
+
+""" Дан список интов, повторяющихся элементов в списке нет. Нужно преобразовать это множество
+в строку, сворачивая соседние по числовому ряду числа в диапазоны.
+Примеры:
+[1,4,5,2,3,9,8,11,0] => "0-5,8-9,11"
+[1,4,3,2] => "1-4"
+[1,4] => "1,4" """
+
+
+""" some_list = [1, 4, 3, 9, 8, 11, 0, 13, 5, 12]
+some_list.sort()
+result_list = []
+temp_list = []
+print(some_list)
+for i in range(0, len(some_list)-1):
+    if some_list[i + 1] - some_list[i] == 1:
+        temp_list.append(some_list[i])
+    else:
+        temp_list.append(some_list[i])
+        result_list.append(temp_list)
+        temp_list = []
+if temp_list:
+    result_list.append(temp_list)
+
+if some_list[-1] - some_list[-2] == 1:
+    result_list[-1].append(some_list[-1])
+else:
+    result_list.append([some_list[-1]])
+
+print(result_list) """
